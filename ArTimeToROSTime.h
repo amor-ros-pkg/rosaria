fbac3708e1498306fedc8a86fb413906ce8c1687
Reed Hedges <reed@interreality.org>
2015-09-18 16:55:32 -0400

    utility function to convert ARIA ArTime object to ros::Time

---
+++ ArTimeToROSTime.h
@@ -0,0 +1,19 @@
+
+#ifndef ARTIMETOROSTIMESTAMP_H
+#define ARTIMETOROSTIMESTAMP_H
+
+#include <ros/ros.h>
+#include "ariaUtil.h"
+
+ros::Time convertArTimeToROS(const ArTime& t)
+{
+  // ARIA/ARNL times are in reference to an arbitrary starting time, not OS
+  // clock, so find the time elapsed between now and t
+  // to adjust the time stamp in ROS time vs. now accordingly.
+  ArTime arianow;
+  const double dtsec = (double) t.mSecSince(arianow) / 1000.0;
+  //printf("was %f seconds ago\n", dtsec);
+  return ros::Time(ros::Time::now().toSec() - dtsec);
+}
+
+#endif
